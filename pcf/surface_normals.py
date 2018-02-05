3ea4b87df83f3b014415bc120e518bd404a103e4
Rick Armstrong <waitingfortheelectrician@gmail.com>
2018-02-04 22:18:06 -0800

    Basic plane segment generation works.

--- pcf/surface_normals.py
+++ pcf/surface_normals.py
@@ -1,4 +1,16 @@
 import numpy as np
+import numpy.linalg as la
+
+
+def angle(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2', without
+    screwing up near 0 and 90 degrees.
+    From https://newtonexcelbach.com/2014/03/01/
+        the-angle-between-two-vectors-python-version/"""
+    cosang = np.dot(v1, v2)
+    sinang = la.norm(np.cross(v1, v2))
+    return np.arctan2(sinang, cosang)
+
 
 if __name__ == '__main__':
     pass
--- test/surface_normals_test.py
+++ test/surface_normals_test.py
@@ -1,5 +1,7 @@
 import laspy
 import numpy as np
+from pcf.surface_normals import angle as vangle
+import pcf.transformations as xf
 
 
 def generate_las(xyz, outfile):
@@ -39,20 +41,33 @@ def generate_plane_segment(p, n, l, m):
     Returns:
         np.array: mx3 list of points
     """
-    # Generate an lxl segment of the x-y plane.
+    # Generate an lxl segment of the x-y plane, centered at the origin.
     # Let a(x-x_0) + b(y-y_0) + c(z-z_0) = 0
     # x_0=0, y_0=0, c=0 => ax + by = 0
-    xyz = np.random.rand(m, 3) * l
-    xyz[:, 2] = np.zeros(m)
-    return xyz
+    xyz = np.random.rand(3, m) * l
+    xyz[2, :] = np.zeros(m)  # x-y plane
+    xyz += np.array([-l/2., -l/2., 0]).reshape((3, 1))
+
+    # rotate and translate
+    zaxis = np.array([0., 0., 1.])
+    rot_axis = np.cross(zaxis, n)
+    rot_angle = vangle(zaxis, n)
+    Rt = xf.rotation_matrix(rot_angle, rot_axis)
+    Rt[:3, 3] = p  # insert translation
+    xyz_h = np.vstack((xyz, np.ones((1, m))))
+    xyz_transformed = np.dot(Rt, xyz_h)
+
+    return xyz_transformed[:3, :]
 
 
 def test_generate_plane_segment():
+    origin = np.array([0., 0., 0.])
+    n = np.array([1., 0., 1.])  # x-axis
     l = 1.0
-    m = 100
-    seg = generate_plane_segment((0., 0., 0.), (0., 0., 1.), l, m)
-    assert seg.shape == (m, 3)
+    m = 10000
+    seg = generate_plane_segment(origin, n, l, m)
+    assert seg.shape == (3, m)
 
     # dump the points to a .las so we can view them
-    generate_las(seg, '/home/rick/src/pcf/scripts/output.las')
+    generate_las(seg.T, '/home/rick/src/pcf/scripts/output.las')
 
